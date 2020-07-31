4770482b327ab00f385c0fd115fb897b4089df73
omuricer <guts.inaq@gmail.com>
2020-07-31 18:37:08 +0900

    deploy.shを修正

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rdf docs app/dist
+rm -rdf docs dist
 # cp -rd docs-for-github-pages docs
 npm install
 npx webpack --mode production
 cp -rd src/image dist
-mv dist/* docs
+mv dist docs
 exit
