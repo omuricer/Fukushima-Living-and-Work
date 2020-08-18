2029df2efc335f349212022d768a61dbf95978d8
omuricer <guts.inaq@gmail.com>
2020-08-18 17:54:56 +0900

    FirstViewをそれなりに実装

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       position: "relative",
       backgroundSize: "contain",
-      background: `linear-gradient(to bottom, rgba(246, 255, 0, 0.2), rgba(255, 0, 161, 0.2)), url(${backgroundImage})`,
+      background: `linear-gradient(to bottom, rgba(68, 186, 255, 0.2), rgba(0, 255, 31, 0.2)), url(${backgroundImage})`,
     },
   })
 );
