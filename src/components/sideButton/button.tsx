4fcce85491fad420ea369d26657b10f4b5de9b29
omuricer <guts.inaq@gmail.com>
2020-08-27 17:35:01 +0900

    サイドのボタンを抽象化。react-springでアニメーションを適用

---
+++ src/components/sideButton/button.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { useSpring, animated } from "react-spring";
+import Image from "@/components/form/image";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: "gray", // TODO: あとでけす
+      marginBottom: "10px",
+      cursor: "pointer",
+      position: "relative",
+    },
+    image: {
+      width: "100%",
+    },
+    label: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translateY(-50%) translateX(-50%) rotateZ(-90deg)",
+      WebkitTransform: "translateY(-50%) translateX(-50%) rotateZ(-90deg)",
+      whiteSpace: "nowrap",
+      color: "#000000", //TODO: あとでかえる
+    },
+  })
+);
+
+interface IButtonProps extends React.HTMLProps<HTMLDivElement> {
+  isVisible: boolean;
+  visibleDelay: number;
+  image: string;
+}
+const Button: React.FC<IButtonProps> = (props) => {
+  const classes = useStyles();
+
+  const springProps = useSpring({
+    from: {
+      transform: "translateX(100px)",
+    },
+    to: props.isVisible
+      ? {
+          transform: "translateX(0)",
+        }
+      : {
+          transform: "translateX(100px)",
+        },
+    delay: props.isVisible ? props.visibleDelay : 0,
+  });
+
+  // refの型がanimated.divの引数と一致しないため除外
+  const { ref, isVisible, visibleDelay, ...divProps } = { ...props };
+  return (
+    <animated.div className={classes.root} style={springProps} {...divProps}>
+      <Image src={props.image} className={classes.image} />
+      <div className={classes.label}>{props.children}</div>
+    </animated.div>
+  );
+};
+export default Button;
---
+++ src/components/sideButton/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { useSpring, animated } from "react-spring";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    intersectingPoint: {
+      top: "50vh",
+      right: "0",
+      position: "absolute",
+      height: "100%",
+    },
+    sideArea: {
+      top: "10vh",
+      right: "0",
+      width: "40px",
+      display: "flex",
+      zIndex: 1,
+      position: "fixed",
+      flexFlow: "column",
+    },
+  })
+);
+
+interface ISideButtonAreaProps {
+  onVisible: () => void;
+  onHide: () => void;
+}
+const SideButtonArea: React.FC<ISideButtonAreaProps> = (props) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const classes = useStyles();
+  const intersectingPoint = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleObserver: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          props.onVisible();
+          // setIsVisible(true);
+        } else {
+          props.onHide();
+          // setIsVisible(false);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleObserver);
+    if (intersectingPoint.current) {
+      observer.observe(intersectingPoint.current);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div ref={intersectingPoint} className={classes.intersectingPoint} />
+      <div
+        className={classes.sideArea}
+        // style={{ visibility: isVisible ? "visible" : "hidden" }}
+      >
+        {props.children}
+      </div>
+    </React.Fragment>
+  );
+};
+export default SideButtonArea;
