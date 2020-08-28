c5e1a807cf28599968e111330b0d42b10777e896
omuricer <guts.inaq@gmail.com>
2020-08-28 17:44:29 +0900

    LivingModalのデザインを微調整

--- src/components/virtualSpace/resident/modal/livingModal.tsx
+++ src/components/virtualSpace/resident/modal/livingModal.tsx
@@ -4,6 +4,7 @@ import Modal, { IModalProps } from "./index";
 import Typography from "@material-ui/core/Typography";
 import Image from "@/components/form/image";
 import Button from "@material-ui/core/Button";
+import { parallel } from "@/app/libs/thread";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,6 +31,7 @@ const useStyles = makeStyles((theme: Theme) =>
     advidors: {
       display: "flex",
       flexFlow: "wrap",
+      marginBottom: "20px",
     },
   })
 );
@@ -58,10 +60,11 @@ export interface ILivingModalProps extends IModalProps {
   content: LivingContent;
 }
 const LivingModal: React.FC<ILivingModalProps> = (props) => {
+  const [preLoadedImage, setPreLoadedImage] = useState<boolean>(false);
   const classes = useStyles();
 
   const advisors = props.content.advisors.map((a, index) => (
-    <Advisor key={index} color={props.headerColor} {...a} />
+    <Advisor {...a} key={index} color={props.content.headerColor} />
   ));
   const changeModal = (key: string) => {
     props.closeModal();
@@ -93,8 +96,9 @@ export default LivingModal;
 
 const useStylesAdvisor = makeStyles((theme: Theme) =>
   createStyles({
-    a: {
+    button: {
       width: "50%",
+      padding: 0,
     },
     li: {
       display: "flex",
@@ -102,6 +106,7 @@ const useStylesAdvisor = makeStyles((theme: Theme) =>
       borderStyle: "solid",
       borderRadius: "4px",
       margin: "5px",
+      position: "relative",
     },
     image: {
       width: "60px",
@@ -110,6 +115,17 @@ const useStylesAdvisor = makeStyles((theme: Theme) =>
     },
     name: {
       margin: "10px",
+    },
+    shadow: {
+      borderBottom: "solid 4px rgba(0,0,0,0.2)",
+      borderRadius: "7px",
+      position: "absolute",
+      width: "calc(100% + 4px)",
+      height: "30px",
+      top: "calc(100% + 2px)",
+      left: "50%",
+      transform: "translateY(-50%) translateX(-50%)",
+      WebkitTransform: "translateY(-100%) translateX(-50%)",
     },
   })
 );
@@ -123,17 +139,16 @@ export interface IAdvisorProps {
 const Advisor: React.FC<IAdvisorProps> = (props) => {
   const classes = useStylesAdvisor();
   return (
-    <a
-      href={`https://xxx.xxx/aaa/${props.id}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={classes.a}
+    <Button
+      className={classes.button}
+      onClick={() => window.open(`https://xxx.xxx/aaa/${props.id}`)}
     >
       <li className={classes.li} style={{ borderColor: props.color }}>
         <Image src={props.image} className={classes.image} />
         <Typography className={classes.name}>{props.name}</Typography>
+        <div className={classes.shadow} />
       </li>
-    </a>
+    </Button>
   );
 };
 
